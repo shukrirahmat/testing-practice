6174a9c14624ddcc9f2b48a34a9a35d6f18188ef
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-06-14 20:42:13 +0800

    Added more tests to capitalize function

--- capitalize.js
+++ capitalize.js
@@ -1,4 +1,6 @@
 function capitalize(text) {
+  if (text === null) return null;
+  if (typeof text !== "string") return "";
   const first = text.charAt(0).toUpperCase();
   return first + text.slice(1);
 }
--- capitalize.test.js
+++ capitalize.test.js
@@ -1,5 +1,21 @@
 import capitalize from "./capitalize.js";
 
-test("capitalize normal string", () => {
+test("normal string", () => {
     expect(capitalize("hello")).toBe("Hello");
+})
+
+test("non alphabet string", () => {
+    expect(capitalize("31245")).toBe("31245");
+})
+
+test("white space", () => {
+    expect(capitalize(" hello")).toBe(" hello");
+})
+
+test("null", () => {
+    expect(capitalize(null)).toBeNull();
+})
+
+test("non-string", () => {
+    expect(capitalize([1,2])).toBe("");
 })
